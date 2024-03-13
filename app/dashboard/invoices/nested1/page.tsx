e067d58346ead9f8c54a4839e303973e96cb9887
tim.re <tim.re@kakaocorp.com>
2024-03-14 06:19:58 +0900

    first commit until chapter 5

---
+++ app/dashboard/invoices/nested1/page.tsx
@@ -0,0 +1,3 @@
+export default function InvoicePage() {
+    return <p>Nested1 Page</p>;
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lusitana, Noto_Sans_KR, Noto_Serif_KR } from 'next/font/google';
+
+
+export const inter = Inter({subsets: ['latin']});
+export const lusitana = Lusitana({weight: '400', subsets: ['latin']});
+
+export const notoSansKR = Noto_Sans_KR({subsets: ['latin']});
+
+export const notoSerifKR = Noto_Serif_KR({weight: '900', subsets: ['latin']});
